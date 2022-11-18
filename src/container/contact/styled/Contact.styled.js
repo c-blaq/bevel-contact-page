a664070ff9a2a1e198faa2c9b7f5cb39abac926f
Fathi <babayejuadesina@gmail.com>
2022-11-18 02:56:20 +0100

    style: form section responsive styling

--- src/container/contact/styled/Contact.styled.js
+++ src/container/contact/styled/Contact.styled.js
@@ -25,3 +25,96 @@ export const HeroSection = styled.div`
     min-height: 400px;
   }
 `;
+
+export const FormSection = styled.div`
+  h2 {
+    font-weight: 500;
+    font-size: 1.5rem;
+    margin-bottom: 1.25rem;
+  }
+
+  p {
+    color: #4b4b4b;
+    font-size: 14px;
+  }
+
+  form {
+    margin-top: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    font-size: 14px;
+    width: 100%;
+
+    label {
+      font-weight: 500;
+      margin-bottom: 4px;
+    }
+
+    input,
+    textarea {
+      padding: 10px 12px;
+      border: 1px solid #d1d5db;
+      box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+      border-radius: 6px;
+      width: 100%;
+    }
+
+    textarea {
+      height: 224px;
+    }
+
+    button {
+      padding: 1.25rem 0;
+      width: 60%;
+      color: #94a3b8;
+      margin-top: 1rem;
+      font-size: 1.125rem;
+      font-weight: 600;
+      border-radius: 5px;
+      border: none;
+    }
+
+    button:disabled {
+      background: #e2e8f0;
+    }
+  }
+
+  @media (min-width: 768px) {
+    min-width: 470px;
+
+    h2 {
+      font-size: 31px;
+    }
+    p {
+      width: 70%;
+    }
+
+    form {
+      width: 50%;
+
+      textarea {
+        height: 195px;
+      }
+    }
+  }
+
+  @media (min-width: 1200px) {
+    width: 80%;
+
+    h2 {
+      font-size: 40px;
+    }
+    p {
+      font-size: 1rem;
+    }
+
+    form {
+      max-width: 375px;
+
+      textarea {
+        height: 195px;
+      }
+    }
+  }
+`;
