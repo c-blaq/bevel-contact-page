9b49abf37bf18d0f964e88ba080fa9d505869b20
Fathi <babayejuadesina@gmail.com>
2022-11-18 16:13:37 +0100

    feat: header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,136 +1,90 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Button from "@mui/material/Button";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import useScrollTrigger from "@mui/material/useScrollTrigger";
-import styled from "styled-components";
+import { Box, Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+import DrawerComponent from "../Drawer";
 
-const drawerWidth = 240;
-const navItems = ["Home", "Convert", "Currency Profile", "News", "Contact"];
+const NavComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export const Title = styled.h3`
-  font-size: 25px;
-  text-align: ${(props) => props.position.align};
-  margin-top: ${(props) => props.position.marginTop};
-  font-weight: 600;
-  color: #00296b;
-  line-height: 32px;
-  letter-spacing: -0.02em;
-`;
-
-function ElevationScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 70,
-    target: window ? window() : undefined,
-  });
-
-  return React.cloneElement(children, {
-    elevation: trigger ? 1 : 0,
-  });
-}
-
-export default function NavBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+  const HandleDrawerState = () => {
+    setIsOpen(!isOpen);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2, ml: 2 }}>
-        <Title position={{ align: "left" }}>Street Rate</Title>
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "left" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
   return (
-    <Box sx={{ display: "flex", marginBottom: { sm: 10 } }}>
-      <ElevationScroll {...props}>
-        <AppBar elevation={0} sx={{ bgcolor: "white" }} component="nav">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{
-                flexGrow: 1,
-                display: { sm: "block" },
-              }}
-            >
-              <Title position={{ align: "left" }}>Street Rate</Title>
-            </Typography>
-            <IconButton
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{
-                mr: 2,
-                display: { sm: "none" },
-                color: "#00296B",
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              {navItems.map((item) => (
-                <Button key={item} sx={{ color: "#94A3B8" }}>
-                  {item}
-                </Button>
-              ))}
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
+    <Grid
+      sx={{
+        maxWidth: { xs: "90%", sm: "95%", md: "84%" },
+        minHeight: { xs: "56px", sm: "100px" },
+        justifyContent: { xs: "space-between" },
+      }}
+      minHeight="100px"
+      display="flex"
+      margin="0px auto"
+      flexDirection="row"
+    >
+      <Grid display="flex" justifyContent="center" alignItems="center">
+        <Typography
           sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
+            fontSize: {
+              xs: "23px",
+              sm: "17px",
+              md: "24px",
+              lg: "29px",
+              xl: "34px",
             },
+            lineHeight: { xs: "28px", sm: "40px" },
+            fontWeight: { xs: "600", sm: "700" },
           }}
+          color="#00296B"
+          letterSpacing="-0.04em"
+          role="heading"
         >
-          {drawer}
-        </Drawer>
+          Street Rate
+        </Typography>
+      </Grid>
+
+      <Grid
+        alignItems="center"
+        sx={{
+          fontSize: { sm: "12px", md: "16px" },
+          gap: { sm: "9px", md: "20px", lg: "24px" },
+          display: { xs: "none", sm: "flex" },
+          width: { md: "606pxx" },
+        }}
+        fontWeight="400"
+        lineHeight="24px"
+        color="#94A3B8"
+      >
+        <Box gap="6px" display="flex">
+          <img src="assets/images/flag.svg" alt="flagImage" />
+          <img src="assets/images/DownArrow.svg" alt="arrow" />
+        </Box>
+
+        <Typography>Home</Typography>
+
+        <Typography>Convert</Typography>
+
+        <Typography>Currency Profile</Typography>
+
+        <Typography>News</Typography>
+
+        <Typography>Contact</Typography>
+      </Grid>
+
+      <Box
+        p="12px"
+        sx={{ display: { xs: "flex", sm: "none" } }}
+        cursor="pointer"
+        onClick={() => setIsOpen(true)}
+      >
+        <img src="assets/images/MenuIcon.svg" alt="MenuIcon" />
       </Box>
-      <Toolbar />
-    </Box>
+      <DrawerComponent
+        isOpen={isOpen}
+        setIsOpen={HandleDrawerState}
+        navItems={["Home", "Convert", "Currency Profile", "News", "Contact"]}
+      />
+    </Grid>
   );
-}
+};
+
+export default NavComponent;
